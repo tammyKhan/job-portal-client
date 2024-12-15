85b45a7a7857449c9dccb36c68b8a4b6787c47ee
tammykhan <tammykhan543@gmail.com>
2024-12-15 10:27:08 +0600

    Implement form value retrieval in registration form and add Lottie animation

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,38 @@
+import Lottie from 'lottie-react';
 import React from 'react';
+import registerLottie from '../assets/lottie/register.json'
 
 const Register = () => {
+  const handleRegister = e =>{
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email,password);
+  }
   return (
     <div className="hero bg-base-200 min-h-screen">
   <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
+    <div className="text-center lg:text-left w-96">
+      <Lottie animationData={registerLottie}></Lottie>
     </div>
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
     <h1 className="text-4xl ml-5 font-bold">Register now!</h1>
 
-      <form className="card-body">
+      <form onSubmit={handleRegister} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input name='email'
+          type="email" placeholder="email" className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input name='password'
+          type="password" placeholder="password" className="input input-bordered" required />
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
