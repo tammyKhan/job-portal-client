7a567fc8312e514d17833b095d274b5955da20b6
tammykhan <tammykhan543@gmail.com>
2024-12-18 12:12:50 +0600

    Auth redirection with google

--- src/context/SocialLogin.jsx
+++ src/context/SocialLogin.jsx
@@ -1,13 +1,19 @@
 import React, { useContext } from 'react';
 import AuthContext from './AuthContext';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SocialLogin = () => {
   const {signInWithGoogle} = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const from = location.state || '/';
 
   const handleGoogleSignIn = () =>{
     signInWithGoogle()
     .then(result => {
       console.log(result.user)
+      navigate(from)
+
     })
     .catch(error => {
       console.log(error.message)
