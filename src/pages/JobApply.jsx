8ad2f546add36d5794728023ae43683970d58ac0
tammykhan <tammykhan543@gmail.com>
2025-01-14 16:40:45 +0600

    save job application

--- src/pages/JobApply.jsx
+++ src/pages/JobApply.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
+import Swal from 'sweetalert2';
 
 const JobApply = () => {
   const {id} = useParams();
-  console.log(id);
+  const {user} = useAuth();
+  console.log(id, user);
 
   const submitJobApplication = e => {
     e.preventDefault();
@@ -12,11 +15,41 @@ const JobApply = () => {
     const github = form.github.value;
     const resume = form.resume.value;
   
-    console.log(linkedIn, github, resume)
+    console.log(linkedIn, github, resume);
+
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume
+    }
+
+  fetch('http://localhost:3000/job-applications',{
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify(jobApplication)
+  })
+  .then(res => res.json())
+  .then(data => {
+    if(data.insertedId){
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your work has been saved",
+        showConfirmButton: false,
+        timer: 1500
+      });
+    }
+  })
+
   }
 
   return (
     <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto">
+                <h1 className="text-4xl ml-5 text-center font-bold">Apply Now!</h1>
     <form onSubmit={submitJobApplication} className="card-body">
       <div className="form-control">
         <label className="label">
