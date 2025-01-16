2e987924f86c1e47fdc5baecc2260d29aaad5f35
tammykhan <tammykhan543@gmail.com>
2025-01-16 17:57:51 +0600

    Add UI to display user's posted jobs with job title, deadline, and application count

--- src/pages/MyPostedJobs.jsx
+++ src/pages/MyPostedJobs.jsx
@@ -1,19 +1,43 @@
-import React, { useEffect, useState } from 'react';
-import useAuth from '../hooks/useAuth';
+import React, { useEffect, useState } from "react";
+import useAuth from "../hooks/useAuth";
 
 const MyPostedJobs = () => {
   const [jobs, setJobs] = useState([]);
-  const {user} = useAuth();
+  const { user } = useAuth();
 
   useEffect(() => {
-      fetch(`http://localhost:3000/jobs?email=${user.email}`)
-      .then(res => res.json())
-      .then(data => setJobs(data))
-  }, [user.email])
+    fetch(`http://localhost:3000/jobs?email=${user.email}`)
+      .then((res) => res.json())
+      .then((data) => setJobs(data));
+  }, [user.email]);
 
   return (
     <div>
-      <h2 className='text-3xl'>myPostedJobs : {jobs.length}</h2>
+      <h2 className="text-3xl">myPostedJobs : {jobs.length}</h2>
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th></th>
+              <th>Job Title</th>
+              <th>Deadline</th>
+              <th>Application Count</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              jobs.map((job, i) => <tr>
+                <th>{i + 1}</th>
+                <td>{job.title}</td>
+                <td>{job.applicationDeadline}</td>
+                <td>{job.applicationCount}</td>
+              </tr>
+              )
+            }
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
